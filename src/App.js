e4626dfe4c1bdcbb1fdd677af712b6c4e6f629ef
Caio Oliveira <caiolv@me.com>
2020-04-12 11:09:40 -0300

    Adicionar estilos globais

--- src/App.js
+++ src/App.js
@@ -6,10 +6,13 @@ import './config/ReactotronConfig';
 import Routes from './routes';
 import history from './services/history';
 
+import GlobalStyles from './styles/global';
+
 function App() {
   return (
     <Router history={history}>
       <Routes />
+      <GlobalStyles />
     </Router>
   );
 }
