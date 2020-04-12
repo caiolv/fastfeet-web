3efb444aeba30a8f7602c592168653ce5503e87f
Caio Oliveira <caiolv@me.com>
2020-04-12 13:16:00 -0300

    Adicionar validações

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
+
 import logo from '~/assets/logo.svg';
 
 import { Container } from './styles';
+
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Insira um e-mail válido.')
+    .required('O e-mail é obrigatório.'),
+  password: Yup.string().required('A senha é obrigatória.'),
+});
 
 export default function SignIn() {
   function handleSubmit(data) {
@@ -13,7 +22,7 @@ export default function SignIn() {
     <Container>
       <img src={logo} alt="FastFeet" />
 
-      <Form onSubmit={handleSubmit}>
+      <Form schema={schema} onSubmit={handleSubmit}>
         <strong>Seu e-mail</strong>
         <Input name="email" type="email" placeholder="email@example.com" />
         <strong>Sua senha</strong>
--- src/pages/SignIn/styles.js
+++ src/pages/SignIn/styles.js
@@ -44,6 +44,13 @@ export const Container = styled.div`
       }
     }
 
+    span {
+      color: #f64c75;
+      align-self: flex-start;
+      margin: 0 0 10px;
+      font-weight: bold;
+    }
+
     button {
       margin: 5px 0 0;
       height: 44px;
