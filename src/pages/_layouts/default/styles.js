ff6237fd339799b6a4aee7eae040eae584ad05f3
Caio Oliveira <caiolv@me.com>
2020-04-12 10:17:51 -0300

    Adicionar layouts

---
+++ src/pages/_layouts/default/styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import {darken} from 'polished';
+
+export const Wrapper = styled.div`
+  height: 100%;
+  background: #333;
+
+`;
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -2,13 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Route, Redirect } from 'react-router-dom';
 
+import AuthLayout from '../pages/_layouts/auth';
+import DefaultLayout from '../pages/_layouts/default';
+
 
 export default function RouteWrapper({
   component: Component,
   isPrivate,
   ...rest
 }) {
-  const signed = true;
+  const signed = false;
 
   if(!signed && isPrivate) {
     return <Redirect to="/" />
@@ -18,10 +21,16 @@ export default function RouteWrapper({
     return <Redirect to="/deliveries" />
   }
 
+  const Layout = signed ? DefaultLayout: AuthLayout;
+
   return (
     <Route
       {...rest}
-      component={Component}
+      render={props => (
+        <Layout>
+          <Component {...props} />
+        </Layout>
+      )}
     />
   )
 };
