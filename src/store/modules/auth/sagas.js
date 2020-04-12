3949ccf94c1be01f81c8e2c3619f71d7b23b4cfa
Caio Oliveira <caiolv@me.com>
2020-04-12 18:21:05 -0300

    Adicionar mensagens de erro

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,5 +1,5 @@
 import { takeLatest, call, put, all } from 'redux-saga/effects';
-
+import { toast } from 'react-toastify';
 import { signInSuccess, signFailure } from './actions';
 
 import api from '~/services/api';
@@ -20,6 +20,7 @@ export function* signIn({ payload }) {
 
     history.push('/deliveries');
   } catch (err) {
+    toast.error('Falha na autenticação, verifique seus dados.');
     yield put(signFailure());
   }
 }
