888ce8211b03ffa8121c90609681843be9a922fc
Caio Oliveira <caiolv@me.com>
2020-04-13 10:28:05 -0300

    Adicionar nome no Header com base no redux

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import logo from '~/assets/logo.svg';
 import { Container, Content, Profile } from './styles';
 
 export default function Header() {
+  const profile = useSelector((state) => state.user.profile);
+
   return (
     <Container>
       <Content>
@@ -16,7 +19,7 @@ export default function Header() {
         <aside>
           <Profile>
             <div>
-              <strong>Nome do usuário</strong>
+              <strong>{profile.name}</strong>
               <Link to="/">sair do sistema</Link>
             </div>
           </Profile>
