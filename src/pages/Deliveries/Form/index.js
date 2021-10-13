157243a5795ccad2d3660d1c32415d69f16b05ad
Caio Oliveira <caiolv@me.com>
2021-10-13 20:01:37 -0300

    add: fix typo

--- src/pages/Deliveries/Form/index.js
+++ src/pages/Deliveries/Form/index.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { toast } from 'react-toastify';
 
-import UnForm from '~/components/UnForm';
+import UnForm from '~/components/Unform';
 import Input from '~/components/Input';
 import SelectInput from './SelectInput';
 
